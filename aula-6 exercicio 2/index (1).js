cec0873e25f4f4b2791b21c205e56d016ae91d6f
Pietro.S <166620197+Pietro9821@users.noreply.github.com>
2024-04-17 09:52:06 -0300

    Add files via upload

---
+++ aula-6 exercicio 2/index (1).js
@@ -0,0 +1,9 @@
+// exercicio 2
+let resposta1 = prompt(' voce gosta de programacao? ')
+let resposta2= prompt(' voce ja viajou para outro pais? ')
+let resposta3 = ptompt(' voce pratica esportes? ')
+
+console.log( 'respostas :');
+console.log( ' 1.voce gosta de programacao? + resposta1');
+console.log('2. voce ja viajou para outro pais?' + resposta2 );
+console.log( '3.  voce pratica esportes?' + resposta3);
---
+++ aula-6 exercicios 3/index (1).js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 8;
+
+console.log( ' valores iniciais:');
+console.log(' a =', a);
+console.log('b =', b);
+
+let temp= a;
+a = b;
+b = temp;
+
+console.log( ' valores trocados:');
+console.log(' a =', a);
+console.log(' b=', b);
