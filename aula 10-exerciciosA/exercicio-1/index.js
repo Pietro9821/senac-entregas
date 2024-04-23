076b2a825963c54f94512f6905e14490cae6d7e4
Pietro.S <166620197+Pietro9821@users.noreply.github.com>
2024-04-23 11:42:03 -0300

    Delete aula 10-exerciciosA directory

--- aula 10-exerciciosA/exercicio-1/index.js
+++
@@ -1,5 +0,0 @@
-let nome = prompt( ' qual o seu nome?');
-let email= prompt( ' qual o seu e-mail?');
-let minuscula = email.toLowerCase();
-const mensagem = ` o e-mail  usuário ${minuscula} foi cadastrado com sucesso.Seja bem-vindo(a) ${nome}`
-console.log( mensagem)
