8e3d4ecd5f1c94d0b52299bb4ae41424dbb36138
Pietro.S <166620197+Pietro9821@users.noreply.github.com>
2024-04-23 11:13:58 -0300

    Add files via upload

---
+++ aula-10-exercicios/exercicio-2/index.js
@@ -0,0 +1,13 @@
+let comidas = ['arroz','lasanha','pizza','massas','churrasco']
+
+console.log( comidas)
+console.log( ' essas são as minhas comidas favoritas:')
+console.log( comidas[0])
+console.log( comidas[1])
+console.log( comidas[2])
+console.log( comidas[3])
+console.log( comidas[4])
+
+let comidaUsuario = prompt( ' qual a sua comida favorita?')
+ comidas[2]=comidaUsuario
+ console.log( ' nova lista:', comidas)
