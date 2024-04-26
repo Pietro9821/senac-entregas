1ea7716918565aa985594062b702f0ce3be11483
Pietro.S <166620197+Pietro9821@users.noreply.github.com>
2024-04-26 11:48:57 -0300

    Add files via upload

---
+++ aula-12-exercicios/exercicio-3/index.js
@@ -0,0 +1,31 @@
+let numeroUsuario1 = Number( prompt( ' insira um número'))
+let numeroUsuario2 = Number( prompt(' insira outro número'))
+
+function soma (a,b) { 
+    const soma = a + b 
+    return soma
+}
+
+console.log( soma( numeroUsuario1,numeroUsuario2))
+
+
+function subtracao ( a,b) { 
+const diminuir = a -b
+return diminuir
+}
+
+console.log( subtracao(numeroUsuario1,numeroUsuario2))
+
+function multiplicacao (a,b) { 
+    const multiplicar = a*b
+    return multiplicar
+}
+
+console.log( multiplicacao( numeroUsuario1,numeroUsuario2))
+
+function divisao ( a,b ) { 
+const dividir = a/b
+return dividir
+}
+
+console.log( divisao( numeroUsuario1,numeroUsuario2))
